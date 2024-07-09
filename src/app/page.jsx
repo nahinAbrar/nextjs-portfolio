654dec9c9bdc05cb9f468474cbbf96d3272c11ea
Asm Nahin <asmnahin21@gmail.com>
2024-07-09 15:43:36 +0600

    Home Link Updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,14 @@
 "use client";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import Link from "next/link";
 
 const Homepage = () => {
   return (
     <motion.div className="h-full"
-    initial={{y:"-200vh"}}
-    animate={{y:"0%"}}
-    transition={{duration:1}}>
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}>
       <div className="flex flex-col lg:flex-row h-full px-4 sm:px-8 md:px-12 lg:px-28 xl:px-48">
         {/*Image Container*/}
         <div className="h-1/2 lg:h-full lg:w-1/2 relative">
@@ -26,8 +27,14 @@ const Homepage = () => {
           </p>
           {/*Button*/}
           <div className="flex w-full gap-4">
-            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">View My Work</button>
-            <button className="p-4 rounded-lg ring-1 ring-black ">Contact Me</button>
+            <Link href="/portfolio">
+              <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">View My Work</button>
+            </Link>
+
+            <Link href="/contact">
+              <button className="p-4 rounded-lg ring-1 ring-black ">Contact Me</button>
+            </Link>
+
           </div>
 
 
